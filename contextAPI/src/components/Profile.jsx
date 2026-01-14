d7d7485fd4dc2018c2705ccd43002f81fe817753
Mohammad Ahmad <123619899+thizahmad@users.noreply.github.com>
2026-01-14 17:58:09 +0530

    Add files via upload

---
+++ contextAPI/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+const Profile = () => {
+    const {user} = useContext(UserContext)
+  
+    if(!user) return(
+        <div>Please Login</div>
+    )
+    else return(
+        <div>Welcome {user.username}</div>
+    )
+  
+}
+
+export default Profile
